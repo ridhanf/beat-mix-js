07b42b37839b837f917bc24ebb87be5453c4705d
Ridhan Fadhilah <ridhanfadhilah@gmail.com>
2021-01-08 00:14:27 +0700

    fix all error on script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,48 @@
+const createDrumArray = () => new Array(16).fill(false);
+
 // Drum Arrays
-let kicks
-let snares
-let hiHats
-let rideCymbals
+let kicks = createDrumArray();
+let snares = createDrumArray();
+let hiHats = createDrumArray();
+let rideCymbals = createDrumArray();
+
+const whichDrum = (name) => {
+  switch (name) {
+    case 'kicks':
+      return kicks;
+    case 'snares':
+      return snares;
+    case 'hiHats':
+      return hiHats;
+    case 'rideCymbals':
+      return rideCymbals;
+    default:
+      return;
+  }
+};
+
+const toggleDrum = (drumName, index) => {
+  const drum = whichDrum(drumName);
+  if (!drum || index < 0 || index > 15) {
+    return;
+  }
+  drum[index] = !drum[index];
+};
+
+const clear = (drumName) => {
+  const drum = whichDrum(drumName);
+  if (drum) {
+    drum.fill(false);
+  }
+};
+
+const invert = (drumName) => {
+  const drum = whichDrum(drumName);
+  if (!drum) {
+    return;
+  }
+  for (let i = 0; i < drum.length; i++) {
+    drum[i] = !drum[i];
+  }
+};
 
