13a9488a88ef32652cc0d3df13b125d3907db7fe
Ridhan Fadhilah <ridhanfadhilah@gmail.com>
2021-01-08 00:44:27 +0700

    Fix all the mandatory errors

--- presetHandler.js
+++ presetHandler.js
@@ -2,7 +2,27 @@
 const presets = require('./presets');
 
 // Complete this function:
-const presetHandler = () => {};
+const presetHandler = (method, index, presetArray) => {
+  switch (method) {
+    case 'GET':
+      if (index < 0 || index > 15 ) {
+        return [404]
+      } else {
+        return [200, presets[index]];
+      }
+      break;
+    case 'PUT':
+      if (index < 0 || index > 15) {
+        return [404]
+      } else {
+        presets[index] = presetArray;
+        return [200, presets[index]];
+      }
+      break;
+    default:
+      return [400]
+  }
+};
 
 // Leave this line so that your presetHandler function can be used elsewhere:
 module.exports = presetHandler;
